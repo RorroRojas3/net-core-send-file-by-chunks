e0195c47bbea754e2d8acf9b13943e85d1c1ba4f
Rodrigo Rojas Garcia <rorro.irg@gmail.com>
2020-09-27 23:00:00 -0400

    SendFileByChunks done and working

--- SendFileByChunks/Program.cs
+++ SendFileByChunks/Program.cs
@@ -5,8 +5,10 @@ using System.Threading.Tasks;
 
 namespace SendFileByChunks
 {
-    class Program
+    class SendFileByChunks
     {
+        private const int MAX_BYTES = 1000000;
+
         static async Task Main(string[] args)
         {
             var httpClientHandler = new HttpClientHandler
@@ -17,13 +19,66 @@ namespace SendFileByChunks
                 }
             };
             var httpClient = new HttpClient(httpClientHandler);
+            httpClient.DefaultRequestHeaders.Add("Authorization", "Bearer ");
 
-            var request = new FileByChunksRequest();
-            var file = await File.ReadAllBytesAsync("C://Users/Rorro/Documents/github/net_core_sendFileByChunks/SendFileByChunks/Example.xlsx");
-            request.Bytes = file;
-            var url = "https://localhost:5001/api/v1/File/Chunks";
-            var result = await httpClient.PostAsJsonAsync(url, request);
-            Console.ReadKey();
+            Console.WriteLine($"Reading file");
+            var file = await File.ReadAllBytesAsync(@"Example.xlsx");
+            string fileName = Path.GetFileName(@"Example.xlsx");
+            FileByChunksRequest request = new FileByChunksRequest()
+            {
+                FileName = fileName,
+                FileSize = file.Length.ToString(),
+            };
+
+            Console.WriteLine($"Configurating settings");
+            int totalChunks = (file.Length / MAX_BYTES) + 1;
+            int offSet = 0;
+            string url = "https://localhost:5001/api/v1/File/Chunks";
+            HttpResponseMessage response;
+            string responseContent;
+
+            Console.WriteLine($"Starting to upload by chunks");
+            for (int i = 0; i < totalChunks; i++)
+            {
+                
+                var isLastChunk = i == totalChunks - 1;
+                
+                if (isLastChunk)
+                {
+                    int lastBytes = file.Length - offSet;
+                    byte[] bytes = new byte[lastBytes];           
+                    Array.Copy(file, offSet, bytes, 0, lastBytes);
+                    request.Bytes = bytes;
+                    request.IsLastChunk = isLastChunk;
+                    request.Offset = offSet.ToString();
+                    response = await httpClient.PostAsJsonAsync(url, request);
+                    responseContent = await response.Content.ReadAsStringAsync();
+                    if (!response.IsSuccessStatusCode)
+                    {
+                        Console.WriteLine($"Upload of file unsuccesfull, Status: {response.StatusCode}, " +
+                            $"Response: {responseContent}");
+                    }
+                    Console.WriteLine($"Uploaded file succefully");
+                }
+                else
+                {
+                    byte[] bytes = new byte[MAX_BYTES];
+                    Array.Copy(file, offSet, bytes, 0, MAX_BYTES);
+                    request.Bytes = bytes;
+                    request.IsLastChunk = isLastChunk;
+                    request.Offset = offSet.ToString();
+                    response = await httpClient.PostAsJsonAsync(url, request);
+                    responseContent = await response.Content.ReadAsStringAsync();
+                    if (!response.IsSuccessStatusCode)
+                    {
+                        Console.WriteLine($"Upload of file unsuccesfull, Status: {response.StatusCode}, " +
+                            $"Response: {responseContent}");
+                    }
+
+                    offSet += MAX_BYTES;
+                    Console.WriteLine($"Uploaded {offSet} out of {file.Length}");
+                }             
+            }       
         }
     }
 }
